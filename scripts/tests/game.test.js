70ed5165e40bd038a82417ea25d3c0a1e088f342
Amandio Orepolo <Dinarte.orepolo@gmail.com>
2025-01-04 20:14:19 +0000

    test the keys on game

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -9,10 +9,19 @@ beforeAll(() =>{
     document.open();
     document.write(fileContents);
     document.close();
-})
+});
 
 describe("game object containt correct keys", () =>{
     test("score key exists", () =>{
         expect("score" in game).toBe(true);
-    })
-})
+    });
+    test("currentGame key exists", () =>{
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists",() =>{
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists",() =>{
+        expect("choices" in game).toBe(true);
+    });
+});
