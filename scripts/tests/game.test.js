3ee3a932250d2da60be347105720717277d5425c
Amandio Orepolo <Dinarte.orepolo@gmail.com>
2025-01-04 19:39:51 +0000

    start first testing

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,16 @@
 /**
  * @jest-environment jsdom
  */
+beforeAll(() =>{
+    let fs = require("fs");
+    let fileContent = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContent);
+    document.close();
+})
+
+describe("game object containt correct keys", () =>{
+    test("score key exists", () =>{
+        expect("score" in game).toBe(true);
+    })
+})
